94aea3350942174c89f286a4a5d8971e5adcbdcd
Vadym Barabanov <introvert.png@gmail.com>
2021-09-22 22:53:09 +0300

    Registration & Authentication

--- internal/app/apiserver/server_internal_test.go
+++ internal/app/apiserver/server_internal_test.go
@@ -1,19 +1,115 @@
 package apiserver
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/gorilla/sessions"
 	"github.com/stretchr/testify/assert"
+	"github.com/vadymbarabanov/go-rest-api/internal/app/model"
 	"github.com/vadymbarabanov/go-rest-api/internal/app/store/teststore"
 )
 
 func TestServer_HandleUsersCreate(t *testing.T) {
-	rec := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodPost, "/users", nil)
+	s := newServer(teststore.New(), sessions.NewCookieStore([]byte("test_secret_key")))
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    "user@example.org",
+				"password": "password",
+			},
+			expectedCode: http.StatusCreated,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid params",
+			payload: map[string]string{
+				"email": "invalid",
+			},
+			expectedCode: http.StatusUnprocessableEntity,
+		},
+	}
 
-	s := newServer(teststore.New())
-	s.ServeHTTP(rec, req)
-	assert.Equal(t, rec.Code, http.StatusOK)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			if err := json.NewEncoder(b).Encode(tc.payload); err != nil {
+				t.Fatal(err)
+			}
+			req, _ := http.NewRequest(http.MethodPost, "/users", b)
+			s.ServeHTTP(rec, req)
+			assert.Equal(t, rec.Code, tc.expectedCode)
+		})
+	}
+}
+
+func TestServer_HandleSessionsCreate(t *testing.T) {
+	u := model.TestUser(t)
+	store := teststore.New()
+	if err := store.User().Create(u); err != nil {
+		t.Fatal(t)
+	}
+	s := newServer(store, sessions.NewCookieStore([]byte("test_secret_key")))
+
+	testCases := []struct {
+		name         string
+		payload      interface{}
+		expectedCode int
+	}{
+		{
+			name: "valid",
+			payload: map[string]string{
+				"email":    u.Email,
+				"password": u.Password,
+			},
+			expectedCode: http.StatusOK,
+		},
+		{
+			name:         "invalid payload",
+			payload:      "invalid",
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "invalid email",
+			payload: map[string]string{
+				"email":    "invalid",
+				"password": u.Password,
+			},
+			expectedCode: http.StatusUnauthorized,
+		},
+		{
+			name: "invalid password",
+			payload: map[string]string{
+				"email":    u.Email,
+				"password": "invalid",
+			},
+			expectedCode: http.StatusUnauthorized,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rec := httptest.NewRecorder()
+			b := &bytes.Buffer{}
+			if err := json.NewEncoder(b).Encode(tc.payload); err != nil {
+				t.Fatal(err)
+			}
+			req, _ := http.NewRequest(http.MethodPost, "/sessions", b)
+			s.ServeHTTP(rec, req)
+			assert.Equal(t, rec.Code, tc.expectedCode)
+		})
+	}
 }
--- internal/app/model/user.go
+++ internal/app/model/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID                int
-	Email             string
-	Password          string
-	EncryptedPassword string
+	ID                int    `json:"id"`
+	Email             string `json:"email"`
+	Password          string `json:"password,omitempty"`
+	EncryptedPassword string `json:"-"`
 }
 
 func (u *User) Validate() error {
@@ -19,6 +19,14 @@ func (u *User) Validate() error {
 		validation.Field(&u.Email, validation.Required, is.Email),
 		validation.Field(&u.Password, validation.By(requiredIf(u.EncryptedPassword == "")), validation.Length(6, 100)),
 	)
+}
+
+func (u *User) Sanitize() {
+	u.Password = ""
+}
+
+func (u *User) ComparePassword(password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(u.EncryptedPassword), []byte(password)) == nil
 }
 
 func (u *User) BeforeCreate() error {
